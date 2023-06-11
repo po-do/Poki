486f21b8d1e0239131ba0ebe5b85127e25c66817
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2023-06-11 15:34:58 +0900

    Init Project: Mission Module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WishlistModule } from './wishlist/wishlist.module';
 import { AuthModule } from './auth/auth.module';
+import { MissionModule } from './mission/mission.module';
 
 @Module({
   imports: [
     AuthModule, 
-    WishlistModule
+    WishlistModule, MissionModule
   ],
 
   controllers: [AppController],
