0be026c9923df72306afb55d60b9c5024f6c7750
jy604 <jeongyoungp@gmail.com>
2023-06-12 00:47:21 +0900

    #3 Refactor: 코드 리뷰 후 주석 정리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -36,26 +36,3 @@ export class UserRepository extends Repository<User> {
         }
     }
 }
-
-    // async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    //     const { userid, password, username, type, code } = AuthCredentialsDto;
-
-    //     const salt = await bcrypt.genSalt();
-    //     const hashedPassword = await bcrypt.hash(password, salt);
-        
-    //     const user = this.create({ userid, password: hashedPassword });
-
-    //     try {
-    //         await this.save(user);
-    //     } catch (error) {
-    //         if(error.code === 'ER_DUP_ENTRY') {
-    //             throw new ConflictException('Existing username');
-    //         } else {
-    //             throw new InternalServerErrorException();
-    //         }
-
-
-    //         console.log('error', error);
-    //     }
-    // }
-//}
