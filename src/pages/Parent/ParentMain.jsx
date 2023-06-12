ad083a080c3e5dd41a13ee7166fc719286960342
jaewon4 <kimjaewon0901@gmail.com>
2023-06-12 20:57:03 +0900

    [#2] Feat: ParentMain

---
+++ src/pages/Parent/ParentMain.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./ParentMain.module.css";
+import Calender from "../../components/CalenderElement";
+import MissionRegisterList from "../../components/Mission/MissionRegisterList";
+
+export default function ParentMain() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles["left-part"]}>
+          <div className={styles["podo-dashboard"]}>
+            <div>
+              <img
+                src="https://t1.daumcdn.net/cfile/tistory/991827345BF5441310"
+                alt=""
+              />
+            </div>
+            <div>
+              <p>남은포도송이 개수</p>
+              <p>5/10</p>
+              <p>남은 포도알 개수</p>
+              <p>50/100</p>
+            </div>
+          </div>
+          <div>
+            <MissionRegisterList />
+          </div>
+        </div>
+        <div className={styles["right-part"]}>
+          <div>
+            <Calender />
+          </div>
+          <div>
+            <p>아이의 위시 리스트</p>
+            <ul>
+              <li>카카오 인형</li>
+              <li>카카오 인형</li>
+              <li>카카오 인형</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
