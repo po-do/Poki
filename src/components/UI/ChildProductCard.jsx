7cea022876a6a9a7b547fb3453b4c45247dfda72
suhyun ji <wldmx3@gmail.com>
2023-06-22 13:27:38 +0900

    Fix : 링크 선택 버튼 수정

--- src/components/UI/ChildProductCard.jsx
+++ src/components/UI/ChildProductCard.jsx
@@ -27,7 +27,7 @@ export default function ChildProductCard({item}) {
     </div>
     <div className="flex justify-center m-3">
         <button
-        className="w-3/4 rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+        className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
         onClick={handleDeleteCard}
         >
           삭제
