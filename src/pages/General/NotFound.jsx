c912514f5f267161ba70d29f9868b0c9de4d8265
jaewon4 <kimjaewon0901@gmail.com>
2023-06-22 21:44:34 +0900

    [Feat] NotFound 이미지 수정

--- src/pages/General/NotFound.jsx
+++ src/pages/General/NotFound.jsx
@@ -1,3 +1,6 @@
+import logo from "../../icons/mstile-310x310.png";
+import notFound from "../../icons/notFound.jpg";
+
 export default function Example() {
   return (
     <>
@@ -5,11 +8,7 @@ export default function Example() {
         <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
           <a href="/">
             <span className="sr-only">Your Company</span>
-            <img
-              className="h-10 w-auto sm:h-12"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
+            <img className="h-10 w-auto sm:h-12" src={logo} alt="" />
           </a>
         </header>
         <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
@@ -18,24 +17,16 @@ export default function Example() {
               404
             </p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Page not found 잘 나오니???
+              Page not found
             </h1>
             <p className="mt-6 text-base leading-7 text-gray-600">
               Sorry, we couldn’t find the page you’re looking for.
             </p>
-            <div className="mt-10">
-              <a
-                href="/"
-                className="text-sm font-semibold leading-7 text-indigo-600"
-              >
-                <span aria-hidden="true">&larr;</span> Back to home
-              </a>
-            </div>
           </div>
         </main>
         <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
           <img
-            src="https://images.unsplash.com/photo-1470847355775-e0e3c35a9a2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1825&q=80"
+            src={notFound}
             alt=""
             className="absolute inset-0 h-full w-full object-cover"
           />
