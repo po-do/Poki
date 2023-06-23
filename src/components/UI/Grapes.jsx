949444ce054bfe62697036569b6ef179e901d41e
suhyun ji <wldmx3@gmail.com>
2023-06-23 12:05:02 +0900

    Fix : 메인 페이지 overlay 수정완료

--- src/components/UI/Grapes.jsx
+++ src/components/UI/Grapes.jsx
@@ -1,28 +1,42 @@
-import {React,useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Grapes.module.css";
+import { getBoardStatus } from "../../api/board.js";
 
 export default function Grapes({ GrapesCount, message }) {
-  const [showOverlay, setShowOverlay] = useState(true);
-
+  const [showOverlay, setShowOverlay] = useState(null);
   const handleOverlayClick = () => {
     setShowOverlay(false);
   };
 
+  const getState = async () => {
+    const grapeStatus = await getBoardStatus();
+    if (!grapeStatus.data.grape.blank) {
+      setShowOverlay(true);
+    }
+    
+  };
+
+  useEffect(() => {
+    getState();
+  }, []);
+
   return (
     <div className="relative">
       <div className={styles.container}>
-
-      {/* overlay 창 구현 */}
-      {showOverlay && 
-        <div className="flex-col rounded-2xl absolute inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-10" onClick={handleOverlayClick}>
-          {message.map((item)=> (
-            <>
-              <p className="text-white text-2xl font-bold">{item}</p>
-              <br />
-            </>
-          ))}
-        </div>
-      }
+        {/* overlay 창 구현 */}
+        {showOverlay && (
+          <div
+            className="flex-col rounded-2xl absolute inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-10"
+            onClick={handleOverlayClick}
+          >
+            {message.map((item) => (
+              <React.Fragment key={item}>
+                <p className="text-white text-2xl font-bold">{item}</p>
+                <br />
+              </React.Fragment>
+            ))}
+          </div>
+        )}
 
         <div className={styles.uvas}>
           {/* 줄기 */}
