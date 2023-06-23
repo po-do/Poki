dac021fcc0d9ab5bb3e78a673c82b837ae8d0414
suyoung049 <suyoung049@gmail.com>
2023-06-24 03:43:50 +0900

    #21 Fix: RTC 변경

--- src/video-chat/video-chat.gateway.ts
+++ src/video-chat/video-chat.gateway.ts
@@ -7,6 +7,13 @@ import * as config from 'config';
 
 const conrsConfig = config.get('cors');
 
+const connect_socket: {socket_nickname: string, socket_id: string;}[]= [];
+
+class ExtendedSocket extends Socket {
+  nickname: string;
+}
+
+
 @WebSocketGateway({
   namespace: 'video-chat',
     cors: {
@@ -35,9 +42,24 @@ export class VideoChatGateway implements OnGatewayInit, OnGatewayConnection, OnG
   @SubscribeMessage('setUserName')
   async handleSetUserName(
     @MessageBody() data: {user_id: string},
-    @ConnectedSocket() socket: Socket
+    @ConnectedSocket() socket: ExtendedSocket
     ) {
+      const existingSocketIndex = connect_socket.findIndex((s) => s.socket_nickname === data.user_id);
+
+      if (existingSocketIndex === -1) {
+        socket.nickname = data.user_id;
+        connect_socket.push({
+          socket_nickname: socket.nickname,
+          socket_id: socket.id,
+        });
+      };
+
     await this.videoChatService.createSocketConnection(data.user_id, socket.id);
+  }
+
+  getSocketIdByNickname(nickname: string, socketList: { socket_nickname: string, socket_id: string; }[]): string | undefined {
+    const socket = socketList.find((s) => s.socket_nickname === nickname);
+    return socket ? socket.socket_id : undefined;
   }
 
   @SubscribeMessage('disconnect')
@@ -55,27 +77,46 @@ export class VideoChatGateway implements OnGatewayInit, OnGatewayConnection, OnG
 
   @SubscribeMessage('callUser')
   async handleCallUser(
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: ExtendedSocket,
     @MessageBody() data: {userToCall: string; signalData: any; from: any; name: string}) {
       console.log('calluser')
-    const {userToCall, signalData, from, name} = data;
-
-    try {
-      const userToCallId = await this.videoChatService.findConnectionByUserId(userToCall);
-
-      if (userToCallId) {
-        this.server.to(userToCallId).emit("callUser", { signal: signalData, from, name })
-      } else {
+      const {userToCall, signalData, from, name} = data;
+      console.log(userToCall)
+      console.log(connect_socket)
+      
+      try{
+        const socketId = this.getSocketIdByNickname(userToCall, connect_socket);
+        console.log(socketId)
+        if (socketId) {
+          this.server.to(socketId).emit("callUser", {signal: signalData, from, name})
+        } else {
+          socket.emit("noUserToCall", userToCall);
+        } 
+      } catch (error) {
         socket.emit("noUserToCall", userToCall);
       }
-    } catch (error) {
-      socket.emit("noUserToCall", userToCall);
-    }
+
+      //connect_socket 초기화
+      connect_socket.splice(0, connect_socket.length);
+      console.log(connect_socket)
+
+      // try {
+      //   const userToCallId = await this.videoChatService.findConnectionByUserId(userToCall);
+  
+      //   if (userToCallId) {
+      //     this.server.to(userToCallId).emit("callUser", { signal: signalData, from, name })
+      //   } else {
+      //     socket.emit("noUserToCall", userToCall);
+      //   }
+      // } catch (error) {
+      //   socket.emit("noUserToCall", userToCall);
+      // }
+
   }
 
   @SubscribeMessage('answerCall')
   handleAnswerCall(
-    @ConnectedSocket() socket: Socket, 
+    @ConnectedSocket() socket: ExtendedSocket, 
     @MessageBody() data: {to: string, signal: any}) {
     const {to, signal} = data;
 		this.server.to(to).emit("callAccepted", signal)
