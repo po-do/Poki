35c08aa5cdce4cb4142393879747108c485fba06
suhyun ji <wldmx3@gmail.com>
2023-06-25 14:00:38 +0900

    Fix : 헤더 재 수정2

--- src/pages/children/ChildFormat.jsx
+++ src/pages/children/ChildFormat.jsx
@@ -463,20 +463,20 @@ export default function ChildFormat() {
           {/* 헤더 */}
           <div className="lg:pl-72">
             <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-              <button
+              {/* <button
                 type="button"
                 className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
                 onClick={() => setSidebarOpen(true)}
               >
                 <span className="sr-only">Open sidebar</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-              </button>
+              </button> */}
 
               {/* Separator */}
-              <div
+              {/* <div
                 className="h-6 w-px bg-gray-900/10 lg:hidden"
                 aria-hidden="true"
-              />
+              /> */}
 
               <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
                 <div className="relative flex flex-1"></div>
