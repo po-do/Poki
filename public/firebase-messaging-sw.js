3de1bd79fb2a3341ef34a2a0cc6a50fc54cd64b4
suhyun ji <wldmx3@gmail.com>
2023-06-25 18:29:07 +0900

    Fix : 환경 변수 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,13 +4,14 @@ importScripts(
 );
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM06te35Tjgkuur7cgf04Snqzy5WUnu6s",
+  //프로젝트 설정 > 일반 > 하단의 내 앱 부분 복사
+  apiKey: process.env.REACT_APP_FIRE_API_KEY,
   authDomain: "poki-c90ad.firebaseapp.com",
   projectId: "poki-c90ad",
   storageBucket: "poki-c90ad.appspot.com",
-  messagingSenderId: "313209790307",
-  appId: "1:313209790307:web:23aa00b8b71edac4aa3d56",
-  measurementId: "G-J2QB8LKF0P"
+  messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRE_APP_ID,
+  measurementId: process.env.REACT_APP_FIRE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
