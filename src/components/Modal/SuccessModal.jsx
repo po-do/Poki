3b84e2d2ed86ab5021ea5df340b14fe903a823e2
suhyun ji <wldmx3@gmail.com>
2023-06-27 20:30:17 +0900

    Fix : 성공 모달 수정

--- src/components/Modal/SuccessModal.jsx
+++ src/components/Modal/SuccessModal.jsx
@@ -1,32 +1,71 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/24/outline";
 
 export default function SuccessModal({ closeModal, message }) {
+  const [open] = useState(true);
 
   const setCloseModal = () => {
-    closeModal(); // To close the modal, you can call closeModal()
+    closeModal(); // 모달을 닫기 위해 setOpen(false) 호출
   };
 
   return (
-    <div className="bg-gray-500 bg-opacity-75 transition-opacity absolute inset-0 flex items-center justify-center">
-      <div className="bg-white rounded-lg px-4 pb-4 pt-5 text-left shadow-xl sm:p-6 sm:w-full sm:max-w-sm">
-        <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-          <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-        </div>
-        <div className="mt-3 text-center sm:mt-5">
-          <div className="text-base font-semibold leading-6 text-gray-900">
-            {message}
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setCloseModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                    <CheckIcon
+                      className="h-6 w-6 text-green-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-gray-900"
+                    >
+                      {message}
+                    </Dialog.Title>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() => setCloseModal()}
+                  >
+                    확인
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
-        <div className="mt-5 sm:mt-6">
-          <button
-            type="button"
-            className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={setCloseModal}
-          >
-            확인
-          </button>
-        </div>
-      </div>
-    </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
