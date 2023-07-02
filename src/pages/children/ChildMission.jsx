d4071e6473e2ce7554efdd0835cbe36dc99d3663
jaewon4 <kimjaewon0901@gmail.com>
2023-07-02 16:29:01 +0900

    [Feat] ChildMission페이지 추가

---
+++ src/pages/children/ChildMission.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChildMissionList from "../../components/Mission/ChildMissionList";
+
+export default function ChildMission() {
+  return (
+    <>
+      <ChildMissionList />
+    </>
+  );
+}
