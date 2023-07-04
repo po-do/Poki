c7b257b776173b484d1113ba3f1d7563ea2d4fd6
jaewon4 <kimjaewon0901@gmail.com>
2023-07-04 15:15:38 +0900

    [Fix] bug fix

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -248,12 +248,6 @@ export default function Video() {
           </div>
         </div>
       </div>
-
-      <div className="px-4 py-2">
-        <p className="mt-2 text-lg text-gray-700">
-          부모님과 영상통화를 진행해요~ 😀
-        </p>
-      </div>
       <div className="p-4 mt-2 md:m-4">
         <div
           className={classNames(
